f180db14f4df16f4b667bafb68fcef6dd4eeaa24
manas <mastermanas805@gmail.com>
2021-05-03 11:16:01 +0530

    Updates

---
+++ precache-manifest.bc4a5ba3e2ec920a9ca19180677f34a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e8b3a6a62354419793d3d9e42a48e4",
+    "url": "/AirNow/index.html"
+  },
+  {
+    "revision": "f2f769a758f0cb97561c",
+    "url": "/AirNow/static/css/main.86ec819c.chunk.css"
+  },
+  {
+    "revision": "21dc03416b71f9f1656b",
+    "url": "/AirNow/static/js/2.9dd573d7.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/AirNow/static/js/2.9dd573d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f769a758f0cb97561c",
+    "url": "/AirNow/static/js/main.9ca6ad72.chunk.js"
+  },
+  {
+    "revision": "565f233255895a620c08",
+    "url": "/AirNow/static/js/runtime-main.e90b3f13.js"
+  }
+]);
